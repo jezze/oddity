0f2426d95a409c6edbff58d35f867954f8cc3791
Jens Nyberg <jens.nyberg@gmail.com>
2020-08-05 19:58:25 +0200

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/ztore.c
+++ src/ztore.c
@@ -14,19 +14,33 @@
 #define SCREEN_HEIGHT                   240
 #define SCREEN_BPP                      32
 
+static void ztore_init(void)
+{
+
+    backend_init(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP);
+    backend_loadbackground("back.png");
+    backend_loadfont("habbo.ttf");
+
+}
+
+static void ztore_destroy(void)
+{
+
+    backend_unloadfont();
+    backend_unloadbackground();
+    backend_destroy();
+
+}
+
 void ztore_exec(char *name)
 {
 
     char command[64];
 
     snprintf(command, 64, "opkrun /media/data/apps/%s", name);
-    backend_unloadfont();
-    backend_unloadbackground();
-    backend_destroy();
+    ztore_destroy();
     system(command);
-    backend_init(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP);
-    backend_loadbackground("back.png");
-    backend_loadfont("habbo.ttf");
+    ztore_init();
 
 }
 
@@ -37,14 +51,10 @@ int main(int argc, char **argv)
 
     file_init();
     db_init();
-    backend_init(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP);
-    backend_loadbackground("back.png");
-    backend_loadfont("habbo.ttf");
+    ztore_init();
     view_load(&front->base);
     view_loop();
-    backend_unloadfont();
-    backend_unloadbackground();
-    backend_destroy();
+    ztore_destroy();
 
     return 0;
 
