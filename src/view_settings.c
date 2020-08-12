ad287cc283778f8a011a3bbde22d2fac7dcdb13e
Jens Nyberg <jens.nyberg@gmail.com>
2020-08-12 20:57:11 +0200

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/view_settings.c
+++ src/view_settings.c
@@ -21,13 +21,10 @@ static void place(unsigned int w, unsigned int h)
     widget_area_place(&areas[1], 0, 0, w, h);
     widget_area_place(&areas[2], 0, 0, w, h);
     widget_area_place(&areas[3], 0, 0, w, h);
-    widget_area_place(&areas[4], 0, 0, w, h);
-    widget_area_place(&areas[5], 0, 0, w, h);
     widget_text_place(&texts[0], areas[0].size.x, areas[0].size.y, areas[0].size.w, areas[0].size.h);
     widget_text_place(&texts[1], areas[1].size.x, areas[1].size.y, areas[1].size.w, areas[1].size.h);
     widget_text_place(&texts[2], areas[2].size.x, areas[2].size.y, areas[2].size.w, areas[2].size.h);
     widget_text_place(&texts[3], areas[3].size.x, areas[3].size.y, areas[3].size.w, areas[3].size.h);
-    widget_text_place(&texts[4], areas[4].size.x, areas[4].size.y, areas[4].size.w, areas[4].size.h);
 
 }
 
@@ -39,7 +36,6 @@ static void render(unsigned int ticks)
     widget_text_render(&texts[1]);
     widget_text_render(&texts[2]);
     widget_text_render(&texts[3]);
-    widget_text_render(&texts[4]);
 
 }
 
@@ -55,13 +51,13 @@ static void button(unsigned int key)
         break;
 
     case KEY_UP:
-        if (active > 1)
+        if (active > 0)
             active--;
 
         break;
 
     case KEY_DOWN:
-        if (active < 4)
+        if (active < 3)
             active++;
 
         break;
@@ -75,7 +71,7 @@ static void load(void)
 
     ztore_setview(place, render, button);
 
-    active = 1;
+    active = 0;
 
 }
 
@@ -89,17 +85,14 @@ void view_settings_setup(void)
 
     view_init(&view, load, config);
     view_register("settings", &view);
-    widget_area_init(&areas[0], 0, 0, 6, 1);
-    widget_area_init(&areas[1], 0, 1, 2, 2);
-    widget_area_init(&areas[2], 2, 1, 2, 2);
-    widget_area_init(&areas[3], 4, 1, 2, 2);
-    widget_area_init(&areas[4], 0, 3, 2, 2);
-    widget_area_init(&areas[5], 2, 3, 2, 2);
-    widget_text_init(&texts[0], TEXT_COLOR_TITLE, TEXT_ALIGN_LEFT, "Settings");
-    widget_text_init(&texts[1], TEXT_COLOR_NORMAL, TEXT_ALIGN_LEFT, "Video");
-    widget_text_init(&texts[2], TEXT_COLOR_NORMAL, TEXT_ALIGN_LEFT, "Audio");
-    widget_text_init(&texts[3], TEXT_COLOR_NORMAL, TEXT_ALIGN_LEFT, "Network");
-    widget_text_init(&texts[4], TEXT_COLOR_NORMAL, TEXT_ALIGN_LEFT, "System");
+    widget_area_init(&areas[0], 0, 0, 2, 2);
+    widget_area_init(&areas[1], 2, 0, 2, 2);
+    widget_area_init(&areas[2], 4, 0, 2, 2);
+    widget_area_init(&areas[3], 0, 2, 2, 2);
+    widget_text_init(&texts[0], TEXT_COLOR_NORMAL, TEXT_ALIGN_LEFT, "Video");
+    widget_text_init(&texts[1], TEXT_COLOR_NORMAL, TEXT_ALIGN_LEFT, "Audio");
+    widget_text_init(&texts[2], TEXT_COLOR_NORMAL, TEXT_ALIGN_LEFT, "Network");
+    widget_text_init(&texts[3], TEXT_COLOR_NORMAL, TEXT_ALIGN_LEFT, "System");
 
 }
 
