751553e7763f7a3f35fb80e0788e0d5a0fd6d568
Jens Nyberg <jens.nyberg@gmail.com>
2020-08-14 00:28:45 +0200

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/view_settings.c
+++ src/view_settings.c
@@ -10,13 +10,7 @@
 
 static struct view view;
 static struct widget_area areas[4];
-static struct widget_text textvideo;
-static struct widget_text textresolution;
-static struct widget_text textaudio;
-static struct widget_text textvolume;
-static struct widget_text textnetwork;
-static struct widget_text textaddress;
-static struct widget_text textsystem;
+static struct widget_text texts[7];
 static struct selection selection;
 
 static void place(unsigned int w, unsigned int h)
@@ -26,13 +20,13 @@ static void place(unsigned int w, unsigned int h)
     widget_area_place(&areas[1], 0, 0, w, h);
     widget_area_place(&areas[2], 0, 0, w, h);
     widget_area_place(&areas[3], 0, 0, w, h);
-    widget_text_placein(&textvideo, &areas[0].size);
-    widget_text_placein(&textresolution, &areas[0].size);
-    widget_text_placein(&textaudio, &areas[1].size);
-    widget_text_placein(&textvolume, &areas[1].size);
-    widget_text_placein(&textnetwork, &areas[2].size);
-    widget_text_placein(&textaddress, &areas[2].size);
-    widget_text_placein(&textsystem, &areas[3].size);
+    widget_text_placein(&texts[0], &areas[0].size);
+    widget_text_placein(&texts[1], &areas[0].size);
+    widget_text_placein(&texts[2], &areas[1].size);
+    widget_text_placein(&texts[3], &areas[1].size);
+    widget_text_placein(&texts[4], &areas[2].size);
+    widget_text_placein(&texts[5], &areas[2].size);
+    widget_text_placein(&texts[6], &areas[3].size);
 
 }
 
@@ -40,13 +34,13 @@ static void render(unsigned int ticks)
 {
 
     widget_area_render(selection.active->data);
-    widget_text_render(&textvideo);
-    widget_text_render(&textresolution);
-    widget_text_render(&textaudio);
-    widget_text_render(&textvolume);
-    widget_text_render(&textnetwork);
-    widget_text_render(&textaddress);
-    widget_text_render(&textsystem);
+    widget_text_render(&texts[0]);
+    widget_text_render(&texts[1]);
+    widget_text_render(&texts[2]);
+    widget_text_render(&texts[3]);
+    widget_text_render(&texts[4]);
+    widget_text_render(&texts[5]);
+    widget_text_render(&texts[6]);
 
 }
 
@@ -76,13 +70,13 @@ void view_settings_setup(void)
     widget_area_init(&areas[1], 0, 1, 8, 1);
     widget_area_init(&areas[2], 0, 2, 8, 1);
     widget_area_init(&areas[3], 0, 3, 8, 1);
-    widget_text_init(&textvideo, TEXT_COLOR_SELECT, TEXT_ALIGN_LEFT, "Video");
-    widget_text_init(&textresolution, TEXT_COLOR_NORMAL, TEXT_ALIGN_RIGHT, "320x240");
-    widget_text_init(&textaudio, TEXT_COLOR_SELECT, TEXT_ALIGN_LEFT, "Audio");
-    widget_text_init(&textvolume, TEXT_COLOR_NORMAL, TEXT_ALIGN_RIGHT, "60%");
-    widget_text_init(&textnetwork, TEXT_COLOR_SELECT, TEXT_ALIGN_LEFT, "Network");
-    widget_text_init(&textaddress, TEXT_COLOR_NORMAL, TEXT_ALIGN_RIGHT, "10.1.1.2/30");
-    widget_text_init(&textsystem, TEXT_COLOR_SELECT, TEXT_ALIGN_LEFT, "System");
+    widget_text_init(&texts[0], TEXT_COLOR_SELECT, TEXT_ALIGN_LEFT, "Video");
+    widget_text_init(&texts[1], TEXT_COLOR_NORMAL, TEXT_ALIGN_RIGHT, "320x240");
+    widget_text_init(&texts[2], TEXT_COLOR_SELECT, TEXT_ALIGN_LEFT, "Audio");
+    widget_text_init(&texts[3], TEXT_COLOR_NORMAL, TEXT_ALIGN_RIGHT, "60%");
+    widget_text_init(&texts[4], TEXT_COLOR_SELECT, TEXT_ALIGN_LEFT, "Network");
+    widget_text_init(&texts[5], TEXT_COLOR_NORMAL, TEXT_ALIGN_RIGHT, "10.1.1.2/30");
+    widget_text_init(&texts[6], TEXT_COLOR_SELECT, TEXT_ALIGN_LEFT, "System");
     list_add(&selection.list, &areas[0].item);
     list_add(&selection.list, &areas[1].item);
     list_add(&selection.list, &areas[2].item);
