1b516f64c75a9b9db9951c9482de6d914c92f8b4
Jens Nyberg <jens.nyberg@gmail.com>
2020-08-14 00:24:42 +0200

    more work
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/view_repolist.c
+++ src/view_repolist.c
@@ -4,15 +4,16 @@
 #include "box.h"
 #include "text.h"
 #include "list.h"
-#include "menu.h"
-#include "db.h"
 #include "view.h"
+#include "db.h"
 #include "ztore.h"
+#include "widget.h"
+#include "selection.h"
 
 static struct view view;
-static struct menu menu;
-static struct box menubox;
-static struct menuitem menuitems[4];
+static struct widget_area areas[4];
+static struct widget_text texts[8];
+static struct selection selection;
 static char all[16];
 static char new[16];
 static char updated[16];
@@ -22,31 +23,82 @@ static struct db_applist applist;
 static void place(unsigned int w, unsigned int h)
 {
 
-    box_setpartsize(&menubox, w, h, 0, 0, 1, 1);
+    widget_area_place(&areas[0], 0, 0, w, h);
+    widget_area_place(&areas[1], 0, 0, w, h);
+    widget_area_place(&areas[2], 0, 0, w, h);
+    widget_area_place(&areas[3], 0, 0, w, h);
+    widget_text_placein(&texts[0], &areas[0].size);
+    widget_text_placein(&texts[1], &areas[0].size);
+    widget_text_placein(&texts[2], &areas[1].size);
+    widget_text_placein(&texts[3], &areas[1].size);
+    widget_text_placein(&texts[4], &areas[2].size);
+    widget_text_placein(&texts[5], &areas[2].size);
+    widget_text_placein(&texts[6], &areas[3].size);
+    widget_text_placein(&texts[7], &areas[3].size);
 
 }
 
 static void render(unsigned int ticks)
 {
 
-    menu_render(&menu, &menubox);
+    widget_area_render(selection.active->data);
+    widget_text_render(&texts[0]);
+    widget_text_render(&texts[1]);
+    widget_text_render(&texts[2]);
+    widget_text_render(&texts[3]);
+    widget_text_render(&texts[4]);
+    widget_text_render(&texts[5]);
+    widget_text_render(&texts[6]);
+    widget_text_render(&texts[7]);
 
 }
 
 static void button(unsigned int key)
 {
 
-    menu_button(&menu, key);
+    selection_setclosest(&selection, key);
 
-    switch (key)
+    if (key == KEY_A && selection_isactive(&selection, &areas[0].item))
     {
 
-    case KEY_B:
-        view_quit("repolist");
-
-        break;
+        db_freeapps(&applist);
+        db_loadapps(&applist);
+        view_config("applist", "list", &applist);
+        selection_select(&selection, key, "repolist", "applist");
 
     }
+
+    if (key == KEY_A && selection_isactive(&selection, &areas[1].item))
+    {
+
+        db_freeapps(&applist);
+        db_loadappswithstate(&applist, 3);
+        view_config("applist", "list", &applist);
+        selection_select(&selection, key, "repolist", "applist");
+
+    }
+
+    if (key == KEY_A && selection_isactive(&selection, &areas[2].item))
+    {
+
+        db_freeapps(&applist);
+        db_loadappswithstate(&applist, 1);
+        view_config("applist", "list", &applist);
+        selection_select(&selection, key, "repolist", "applist");
+
+    }
+
+    if (key == KEY_A && selection_isactive(&selection, &areas[3].item))
+    {
+
+        db_freeapps(&applist);
+        db_loadappswithstate(&applist, 2);
+        view_config("applist", "list", &applist);
+        selection_select(&selection, key, "repolist", "applist");
+
+    }
+
+    selection_return(&selection, key, "repolist");
 
 }
 
@@ -57,68 +109,33 @@ static void load(void)
     snprintf(new, 16, "%u items", db_countappswithstate(1));
     snprintf(updated, 16, "%u items", db_countappswithstate(2));
     snprintf(installed, 16, "%u items", db_countappswithstate(3));
-
-    menu.items[0].info = all;
-    menu.items[1].info = installed;
-    menu.items[2].info = new;
-    menu.items[3].info = updated;
-
     ztore_setview(place, render, button);
 
-}
-
-static void config(char *key, void *value)
-{
-
-}
-
-static void menu_onselect(unsigned int index)
-{
-
-    db_freeapps(&applist);
-
-    switch (index)
-    {
-
-    case 0:
-        db_loadapps(&applist);
-
-        break;
-
-    case 1:
-        db_loadappswithstate(&applist, 3);
-
-        break;
-
-    case 2:
-        db_loadappswithstate(&applist, 1);
-
-        break;
-
-    case 3:
-        db_loadappswithstate(&applist, 2);
-
-        break;
-
-    }
-
-    view_config("applist", "list", &applist);
-    view_load("applist", "repolist");
+    selection.active = selection.list.head;
 
 }
 
 void view_repolist_setup(void)
 {
 
-    view_init(&view, "repolist", load, config);
-    box_init(&menubox);
-    menu_init(&menu, menuitems, 4, menu_onselect);
-    menu_inititem(&menuitems[0], "All", "0 items");
-    menu_inititem(&menuitems[1], "Installed", "0 items");
-    menu_inititem(&menuitems[2], "New", "0 items");
-    menu_inititem(&menuitems[3], "Updated", "0 items");
-    menu_setrow(&menu, 0);
+    view_init(&view, "repolist", load, 0);
     view_register(&view);
+    widget_area_init(&areas[0], 0, 0, 8, 1);
+    widget_area_init(&areas[1], 0, 1, 8, 1);
+    widget_area_init(&areas[2], 0, 2, 8, 1);
+    widget_area_init(&areas[3], 0, 3, 8, 1);
+    widget_text_init(&texts[0], TEXT_COLOR_SELECT, TEXT_ALIGN_LEFT, "All");
+    widget_text_init(&texts[1], TEXT_COLOR_NORMAL, TEXT_ALIGN_RIGHT, all);
+    widget_text_init(&texts[2], TEXT_COLOR_SELECT, TEXT_ALIGN_LEFT, "Installed");
+    widget_text_init(&texts[3], TEXT_COLOR_NORMAL, TEXT_ALIGN_RIGHT, installed);
+    widget_text_init(&texts[4], TEXT_COLOR_SELECT, TEXT_ALIGN_LEFT, "New");
+    widget_text_init(&texts[5], TEXT_COLOR_NORMAL, TEXT_ALIGN_RIGHT, new);
+    widget_text_init(&texts[6], TEXT_COLOR_SELECT, TEXT_ALIGN_LEFT, "Updated");
+    widget_text_init(&texts[7], TEXT_COLOR_NORMAL, TEXT_ALIGN_RIGHT, updated);
+    list_add(&selection.list, &areas[0].item);
+    list_add(&selection.list, &areas[1].item);
+    list_add(&selection.list, &areas[2].item);
+    list_add(&selection.list, &areas[3].item);
 
 }
 
