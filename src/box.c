81e238709a34d5709eb856261e4986302dd76c3c
Jens Nyberg <jens.nyberg@gmail.com>
2020-08-15 14:22:10 +0200

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/box.c
+++ src/box.c
@@ -2,18 +2,6 @@
 #include "define.h"
 #include "box.h"
 
-#define PADDING                         12
-
-void box_setpartsize(struct box *box, unsigned int pw, unsigned int ph, unsigned int x, unsigned int y, unsigned int w, unsigned int h)
-{
-
-    box->x = x * pw + PADDING;
-    box->y = y * ph + PADDING;
-    box->w = w * pw - (2 * PADDING);
-    box->h = h * ph - (2 * PADDING);
-
-}
-
 void box_init(struct box *box)
 {
 
--- src/box.h
+++ src/box.h
@@ -8,5 +8,4 @@ struct box
 
 };
 
-void box_setpartsize(struct box *box, unsigned int pw, unsigned int ph, unsigned int x, unsigned int y, unsigned int w, unsigned int h);
 void box_init(struct box *box);
