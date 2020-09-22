54c444e10a27d55e7d6a06247b3ab1793f9f534e
Jens Nyberg <jens.nyberg@gmail.com>
2020-09-22 21:50:33 +0200

    more
    Signed-off-by: Jens Nyberg <jens.nyberg@gmail.com>

--- src/views/applist.c
+++ src/views/applist.c
@@ -2,81 +2,130 @@
 
 static struct view view;
 static struct db_applist applist;
-static unsigned int page;
-static char *names[8];
 
 static void onbutton(unsigned int key)
 {
 
-    unsigned int offset = page * 8;
-    unsigned int i;
-
     view_moveselection(&view, key);
 
-    for (i = offset; i < offset + 8; i++)
+    if (view_isactive(&view, "area0"))
     {
 
-        if (i < applist.count)
-        {
+        main_configview("app", "id", applist.items[0].id);
+        view_select(&view, key, "area0", "app");
 
-/*
-            struct db_app *app = &applist.items[i];
-            unsigned int k = i % 8;
+    }
 
-            if (view_isactive(&view, areas[k].id))
-            {
+    if (view_isactive(&view, "area1"))
+    {
 
-                main_configview("app", "id", app->id);
-                view_select(&view, key, areas[k].id, "app");
+        main_configview("app", "id", applist.items[1].id);
+        view_select(&view, key, "area1", "app");
 
-            }
-*/
+    }
 
-        }
+    if (view_isactive(&view, "area2"))
+    {
+
+        main_configview("app", "id", applist.items[2].id);
+        view_select(&view, key, "area2", "app");
+
+    }
+
+    if (view_isactive(&view, "area3"))
+    {
+
+        main_configview("app", "id", applist.items[3].id);
+        view_select(&view, key, "area3", "app");
+
+    }
+
+    if (view_isactive(&view, "area4"))
+    {
+
+        main_configview("app", "id", applist.items[4].id);
+        view_select(&view, key, "area4", "app");
+
+    }
+
+    if (view_isactive(&view, "area5"))
+    {
+
+        main_configview("app", "id", applist.items[5].id);
+        view_select(&view, key, "area5", "app");
+
+    }
+
+    if (view_isactive(&view, "area6"))
+    {
+
+        main_configview("app", "id", applist.items[6].id);
+        view_select(&view, key, "area6", "app");
+
+    }
+
+    if (view_isactive(&view, "area7"))
+    {
+
+        main_configview("app", "id", applist.items[7].id);
+        view_select(&view, key, "area7", "app");
 
     }
 
     view_unselect(&view, key);
-
-    switch (key)
-    {
-
-    case KEY_LEFT:
-        if (page > 0)
-            page--;
-
-        break;
-
-    case KEY_RIGHT:
-        if (page < (applist.count / 8))
-            page++;
-
-        break;
-
-    }
 
 }
 
 static void onload(unsigned int type)
 {
 
-    unsigned int i;
+    char *names[8];
 
-    page = 0;
+    names[0] = applist.items[0].name;
+    names[1] = applist.items[1].name;
+    names[2] = 0;
+    names[3] = 0;
+    names[4] = 0;
+    names[5] = 0;
+    names[6] = 0;
+    names[7] = 0;
 
-    for (i = 0; i < 8; i++)
+    if (names[0])
     {
 
-        names[i] = "";
+        view_setattr(&view, "area0", "selectable", "true");
+        view_setattr(&view, "text_name0", "data", names[0]);
 
     }
 
-    for (i = 0; i < applist.count; i++)
+    else
     {
 
-        names[i] = applist.items[i].name;
+        view_setattr(&view, "area0", "selectable", "false");
+        view_setattr(&view, "text_name0", "data", 0);
 
     }
+
+    if (names[1])
+    {
+
+        view_setattr(&view, "area1", "selectable", "true");
+        view_setattr(&view, "text_name1", "data", names[1]);
+
+    }
+
+    else
+    {
+
+        view_setattr(&view, "area1", "selectable", "false");
+        view_setattr(&view, "text_name1", "data", 0);
+
+    }
+
+    if (applist.count)
+        view_setattr(&view, "text_noitems", "hidden", "true");
+    else
+        view_setattr(&view, "text_noitems", "hidden", "false");
 
 }
 
@@ -120,23 +169,23 @@ void view_applist_setup(void)
     pool_create_area(&view, "area6", WIDGET_IN_DEFAULT, 0, 6, 12, 1);
     pool_create_area(&view, "area7", WIDGET_IN_DEFAULT, 0, 7, 12, 1);
     pool_create_area(&view, "area_noitems", WIDGET_IN_DEFAULT, 0, 3, 12, 1);
-    pool_create_text(&view, WIDGET_ID_DEFAULT, "area0", TEXT_TYPE_SELECT, TEXT_ALIGN_LEFT, names[0]);
+    pool_create_text(&view, "text_name0", "area0", TEXT_TYPE_SELECT, TEXT_ALIGN_LEFT, 0);
     pool_create_text(&view, WIDGET_ID_DEFAULT, "area0", TEXT_TYPE_NORMAL, TEXT_ALIGN_RIGHT, 0);
-    pool_create_text(&view, WIDGET_ID_DEFAULT, "area1", TEXT_TYPE_SELECT, TEXT_ALIGN_LEFT, names[1]);
+    pool_create_text(&view, "text_name1", "area1", TEXT_TYPE_SELECT, TEXT_ALIGN_LEFT, 0);
     pool_create_text(&view, WIDGET_ID_DEFAULT, "area1", TEXT_TYPE_NORMAL, TEXT_ALIGN_RIGHT, 0);
-    pool_create_text(&view, WIDGET_ID_DEFAULT, "area2", TEXT_TYPE_SELECT, TEXT_ALIGN_LEFT, names[2]);
+    pool_create_text(&view, "text_name2", "area2", TEXT_TYPE_SELECT, TEXT_ALIGN_LEFT, 0);
     pool_create_text(&view, WIDGET_ID_DEFAULT, "area2", TEXT_TYPE_NORMAL, TEXT_ALIGN_RIGHT, 0);
-    pool_create_text(&view, WIDGET_ID_DEFAULT, "area3", TEXT_TYPE_SELECT, TEXT_ALIGN_LEFT, names[3]);
+    pool_create_text(&view, "text_name3", "area3", TEXT_TYPE_SELECT, TEXT_ALIGN_LEFT, 0);
     pool_create_text(&view, WIDGET_ID_DEFAULT, "area3", TEXT_TYPE_NORMAL, TEXT_ALIGN_RIGHT, 0);
-    pool_create_text(&view, WIDGET_ID_DEFAULT, "area4", TEXT_TYPE_SELECT, TEXT_ALIGN_LEFT, names[4]);
+    pool_create_text(&view, "text_name4", "area4", TEXT_TYPE_SELECT, TEXT_ALIGN_LEFT, 0);
     pool_create_text(&view, WIDGET_ID_DEFAULT, "area4", TEXT_TYPE_NORMAL, TEXT_ALIGN_RIGHT, 0);
-    pool_create_text(&view, WIDGET_ID_DEFAULT, "area5", TEXT_TYPE_SELECT, TEXT_ALIGN_LEFT, names[5]);
+    pool_create_text(&view, "text_name5", "area5", TEXT_TYPE_SELECT, TEXT_ALIGN_LEFT, 0);
     pool_create_text(&view, WIDGET_ID_DEFAULT, "area5", TEXT_TYPE_NORMAL, TEXT_ALIGN_RIGHT, 0);
-    pool_create_text(&view, WIDGET_ID_DEFAULT, "area6", TEXT_TYPE_SELECT, TEXT_ALIGN_LEFT, names[6]);
+    pool_create_text(&view, "text_name6", "area6", TEXT_TYPE_SELECT, TEXT_ALIGN_LEFT, 0);
     pool_create_text(&view, WIDGET_ID_DEFAULT, "area6", TEXT_TYPE_NORMAL, TEXT_ALIGN_RIGHT, 0);
-    pool_create_text(&view, WIDGET_ID_DEFAULT, "area7", TEXT_TYPE_SELECT, TEXT_ALIGN_LEFT, names[7]);
+    pool_create_text(&view, "text_name7", "area7", TEXT_TYPE_SELECT, TEXT_ALIGN_LEFT, 0);
     pool_create_text(&view, WIDGET_ID_DEFAULT, "area7", TEXT_TYPE_NORMAL, TEXT_ALIGN_RIGHT, 0);
-    pool_create_text(&view, WIDGET_ID_DEFAULT, "area_noitems", TEXT_TYPE_NORMAL, TEXT_ALIGN_CENTER, "No items found.");
+    pool_create_text(&view, "text_noitems", "area_noitems", TEXT_TYPE_NORMAL, TEXT_ALIGN_CENTER, "No items found.");
     main_registerview(&view);
 
 }
